d0e6bd90314a2f6b7efbd45888f9efa33b9e6459
Vadim <vad.general@mail.ru>
2023-06-03 10:17:38 +0300

    Fix: передавать строку токена, в место строки объекта

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,15 +6,12 @@ module.exports = function (req, res, next) {
     }
     try {
         const token = JSON.parse(req.headers.authorization.split(' ')[1]);
-        console.log(token);
         if (!token.token) {
-            console.log('tut');
             return res.status(401).json({message: "Не авторизован"});
         }
         req.user = jwt.verify(token.token, process.env.SECRET_KEY);
         next();
     } catch (e) {
-        console.log('tut2');
         res.status(401).json({message: "Не авторизован"});
     }
 };
