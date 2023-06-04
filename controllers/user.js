6a996ae735a1de33a939f6dee9605872ee5b8947
Vadim <vad.general@mail.ru>
2023-06-04 17:43:10 +0300

    Feat: исправить способ передачи ошибки на фронтенд

--- controllers/user.js
+++ controllers/user.js
@@ -56,7 +56,7 @@ class UserController {
             const token = await userService.login(username, password);
             return res.status(200).json({token})
         } catch (e) {
-            return res.status(500).json(e.message);
+            return res.status(500).json({message: e.message});
         }
 
     }
